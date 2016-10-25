60c5a4201dc626e87beadb039fa1b128a003547e
Andrew Dong <andrew.mh.dong@gmail.com>
2016-10-25 13:41:08 -0600

    Fixed the error where we couldn't use the robotlibrary.jar in our project. This was fixed by updating the .gradle file and re-building the .gradle file. Also Added buttons/controls to the main layout. We will probably using this as a template moving forward with the project.

--- SpheroHelloWorld/SpheroHelloWorld/app/src/main/java/com/example/andrewdong/spherohelloworld/MainActivity.java
+++ SpheroHelloWorld/SpheroHelloWorld/app/src/main/java/com/example/andrewdong/spherohelloworld/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.andrewdong.spherohelloworld;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.orbotix.ConvenienceRobot;
 
 public class MainActivity extends AppCompatActivity {
 
