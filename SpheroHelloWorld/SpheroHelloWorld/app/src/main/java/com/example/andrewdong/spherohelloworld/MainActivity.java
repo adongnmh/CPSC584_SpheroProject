de765ab3cd9f56687bd225a8dc2c7eaea84faf47
Andrew Dong <andrew.mh.dong@gmail.com>
2016-10-26 15:33:59 -0600

    Updated the .gradle file and the Manifest.xml to include the Bluetooth permission. Application crashed before since we did not include bluetooth permission.

--- SpheroHelloWorld/SpheroHelloWorld/app/src/main/java/com/example/andrewdong/spherohelloworld/MainActivity.java
+++ SpheroHelloWorld/SpheroHelloWorld/app/src/main/java/com/example/andrewdong/spherohelloworld/MainActivity.java
@@ -1,15 +1,161 @@
 package com.example.andrewdong.spherohelloworld;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import com.orbotix.ConvenienceRobot;
+import android.util.Log;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+// Importing Sphero RobotLibrary.jar here:
+import com.orbotix.ConvenienceRobot;
+import com.orbotix.DualStackDiscoveryAgent;
+import com.orbotix.common.*;
+import com.orbotix.le.RobotLE;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, RobotChangedStateListener {
+
+    private static final int REQUEST_CODE_LOCATION_PERMISSION = 42;
+    private static final float ROBOT_VELOCITY = 0.6f;
+
+    private ConvenienceRobot mRobot;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        System.out.println("Hello World");
+
+        DualStackDiscoveryAgent.getInstance().addRobotStateListener( this );
+
+
+        if( Build.VERSION.SDK_INT >= Build.VERSION_CODES.M ) {
+            int hasLocationPermission = checkSelfPermission( Manifest.permission.ACCESS_COARSE_LOCATION );
+            if( hasLocationPermission != PackageManager.PERMISSION_GRANTED ) {
+                Log.e( "Sphero", "Location permission has not already been granted" );
+                List<String> permissions = new ArrayList<String>();
+                permissions.add( Manifest.permission.ACCESS_COARSE_LOCATION);
+                requestPermissions(permissions.toArray(new String[permissions.size()] ), REQUEST_CODE_LOCATION_PERMISSION );
+            } else {
+                Log.d( "Sphero", "Location permission already granted" );
+            }
+        }
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch ( requestCode ) {
+            case REQUEST_CODE_LOCATION_PERMISSION: {
+                for( int i = 0; i < permissions.length; i++ ) {
+                    if( grantResults[i] == PackageManager.PERMISSION_GRANTED ) {
+                        startDiscovery();
+                        Log.d( "Permissions", "Permission Granted: " + permissions[i] );
+                    } else if( grantResults[i] == PackageManager.PERMISSION_DENIED ) {
+                        Log.d( "Permissions", "Permission Denied: " + permissions[i] );
+                    }
+                }
+            }
+            break;
+            default: {
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+            }
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        if( Build.VERSION.SDK_INT < Build.VERSION_CODES.M
+                || checkSelfPermission( Manifest.permission.ACCESS_COARSE_LOCATION ) == PackageManager.PERMISSION_GRANTED ) {
+            startDiscovery();
+        }
+    }
+
+    private void startDiscovery() {
+        //If the DiscoveryAgent is not already looking for robots, start discovery.
+        if( !DualStackDiscoveryAgent.getInstance().isDiscovering() ) {
+            try {
+                DualStackDiscoveryAgent.getInstance().startDiscovery( this );
+            } catch (DiscoveryException e) {
+                Log.e("Sphero", "DiscoveryException: " + e.getMessage());
+            }
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        //If the DiscoveryAgent is in discovery mode, stop it.
+        if( DualStackDiscoveryAgent.getInstance().isDiscovering() ) {
+            DualStackDiscoveryAgent.getInstance().stopDiscovery();
+        }
+
+        //If a robot is connected to the device, disconnect it
+        if( mRobot != null ) {
+            mRobot.disconnect();
+            mRobot = null;
+        }
+
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        DualStackDiscoveryAgent.getInstance().addRobotStateListener( null );
+    }
+
+    @Override
+    public void handleRobotChangedState(Robot robot, RobotChangedStateNotificationType type) {
+        switch (type) {
+            case Online: {
+
+                //If robot uses Bluetooth LE, Developer Mode can be turned on.
+                //This turns off DOS protection. This generally isn't required.
+                if( robot instanceof RobotLE) {
+                    ( (RobotLE) robot ).setDeveloperMode( true );
+                }
+
+                //Save the robot as a ConvenienceRobot for additional utility methods
+                mRobot = new ConvenienceRobot(robot);
+                //Start blinking the robot's LED
+                blink( false );
+                break;
+            }
+        }
+    }
+
+    //Turn the robot LED on or off every two seconds
+    private void blink( final boolean lit ) {
+        if( mRobot == null )
+            return;
+
+        if( lit ) {
+            mRobot.setLed( 0.0f, 0.0f, 0.0f );
+        } else {
+            mRobot.setLed( 0.0f, 0.0f, 1.0f );
+        }
+
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            public void run() {
+                blink(!lit);
+            }
+        }, 2000);
+    }
+
+    /**
+     * TO DO: Add the movements to the Sphero Robot
+     * @param v
+     */
+    @Override
+
+    public void onClick(View v) {
+
     }
 }
