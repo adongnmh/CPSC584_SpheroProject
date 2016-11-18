541953c883ef1bb5b6058f2825d789232614d55e
Andrew Dong <andrew.mh.dong@gmail.com>
2016-11-17 21:33:28 -0700

    I took Nico advice and tried out AForge to get the tracking done. Nico suggested the RGB filter tracking so tracking by a range of color. I currently implemented that so the camera will track by a color. As of now I only have it track differrent shade of 'red' the other colors are still buggy. If you want to try out the tracking just use the red slider and adjust the slider until you see a box around your red Object. Will need  more optimization, but I am currently looking more into edge detection and Blob detection.

---
+++ AForgeCameraTracking/AForgeCameraTracking/AForgeCameraTracking/Form1.Designer.cs
@@ -0,0 +1,281 @@
+﻿namespace AForgeCameraTracking
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.deviceBox = new System.Windows.Forms.ComboBox();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.buttonCapture = new System.Windows.Forms.Button();
+            this.buttonTracking = new System.Windows.Forms.Button();
+            this.trackBarRed = new System.Windows.Forms.TrackBar();
+            this.trackBarGreen = new System.Windows.Forms.TrackBar();
+            this.trackBarBlue = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.numericUpDownRed = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDownGreen = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDownBlue = new System.Windows.Forms.NumericUpDown();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarRed)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarGreen)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarBlue)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownRed)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownGreen)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownBlue)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(42, 12);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(75, 23);
+            this.buttonStart.TabIndex = 0;
+            this.buttonStart.Text = "Start";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // deviceBox
+            // 
+            this.deviceBox.FormattingEnabled = true;
+            this.deviceBox.Location = new System.Drawing.Point(132, 14);
+            this.deviceBox.Name = "deviceBox";
+            this.deviceBox.Size = new System.Drawing.Size(121, 21);
+            this.deviceBox.TabIndex = 1;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox2, 1, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(42, 42);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1056, 461);
+            this.tableLayoutPanel1.TabIndex = 2;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(522, 455);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox2.Location = new System.Drawing.Point(531, 3);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(522, 455);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // buttonCapture
+            // 
+            this.buttonCapture.Location = new System.Drawing.Point(270, 12);
+            this.buttonCapture.Name = "buttonCapture";
+            this.buttonCapture.Size = new System.Drawing.Size(75, 23);
+            this.buttonCapture.TabIndex = 3;
+            this.buttonCapture.Text = "Capture";
+            this.buttonCapture.UseVisualStyleBackColor = true;
+            this.buttonCapture.Click += new System.EventHandler(this.buttonCapture_Click);
+            // 
+            // buttonTracking
+            // 
+            this.buttonTracking.Location = new System.Drawing.Point(352, 12);
+            this.buttonTracking.Name = "buttonTracking";
+            this.buttonTracking.Size = new System.Drawing.Size(75, 23);
+            this.buttonTracking.TabIndex = 4;
+            this.buttonTracking.Text = "TrackObject";
+            this.buttonTracking.UseVisualStyleBackColor = true;
+            this.buttonTracking.Click += new System.EventHandler(this.buttonTracking_Click);
+            // 
+            // trackBarRed
+            // 
+            this.trackBarRed.Location = new System.Drawing.Point(132, 509);
+            this.trackBarRed.Maximum = 255;
+            this.trackBarRed.Name = "trackBarRed";
+            this.trackBarRed.Size = new System.Drawing.Size(511, 45);
+            this.trackBarRed.TabIndex = 5;
+            this.trackBarRed.Scroll += new System.EventHandler(this.trackBarRed_Scroll);
+            // 
+            // trackBarGreen
+            // 
+            this.trackBarGreen.Location = new System.Drawing.Point(132, 571);
+            this.trackBarGreen.Maximum = 255;
+            this.trackBarGreen.Name = "trackBarGreen";
+            this.trackBarGreen.Size = new System.Drawing.Size(511, 45);
+            this.trackBarGreen.TabIndex = 6;
+            this.trackBarGreen.Scroll += new System.EventHandler(this.trackBarGreen_Scroll);
+            // 
+            // trackBarBlue
+            // 
+            this.trackBarBlue.Location = new System.Drawing.Point(132, 634);
+            this.trackBarBlue.Maximum = 255;
+            this.trackBarBlue.Name = "trackBarBlue";
+            this.trackBarBlue.Size = new System.Drawing.Size(511, 45);
+            this.trackBarBlue.TabIndex = 7;
+            this.trackBarBlue.Scroll += new System.EventHandler(this.trackBarBlue_Scroll);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(81, 521);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(27, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Red";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(81, 582);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Green";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(81, 634);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(28, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Blue";
+            // 
+            // numericUpDownRed
+            // 
+            this.numericUpDownRed.Location = new System.Drawing.Point(672, 509);
+            this.numericUpDownRed.Maximum = new decimal(new int[] {
+            255,
+            0,
+            0,
+            0});
+            this.numericUpDownRed.Name = "numericUpDownRed";
+            this.numericUpDownRed.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDownRed.TabIndex = 11;
+            // 
+            // numericUpDownGreen
+            // 
+            this.numericUpDownGreen.Location = new System.Drawing.Point(672, 571);
+            this.numericUpDownGreen.Maximum = new decimal(new int[] {
+            255,
+            0,
+            0,
+            0});
+            this.numericUpDownGreen.Name = "numericUpDownGreen";
+            this.numericUpDownGreen.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDownGreen.TabIndex = 12;
+            // 
+            // numericUpDownBlue
+            // 
+            this.numericUpDownBlue.Location = new System.Drawing.Point(672, 634);
+            this.numericUpDownBlue.Maximum = new decimal(new int[] {
+            255,
+            0,
+            0,
+            0});
+            this.numericUpDownBlue.Name = "numericUpDownBlue";
+            this.numericUpDownBlue.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDownBlue.TabIndex = 13;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1123, 721);
+            this.Controls.Add(this.numericUpDownBlue);
+            this.Controls.Add(this.numericUpDownGreen);
+            this.Controls.Add(this.numericUpDownRed);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.trackBarBlue);
+            this.Controls.Add(this.trackBarGreen);
+            this.Controls.Add(this.trackBarRed);
+            this.Controls.Add(this.buttonTracking);
+            this.Controls.Add(this.buttonCapture);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.deviceBox);
+            this.Controls.Add(this.buttonStart);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarRed)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarGreen)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarBlue)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownRed)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownGreen)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownBlue)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.ComboBox deviceBox;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button buttonCapture;
+        private System.Windows.Forms.Button buttonTracking;
+        private System.Windows.Forms.TrackBar trackBarRed;
+        private System.Windows.Forms.TrackBar trackBarGreen;
+        private System.Windows.Forms.TrackBar trackBarBlue;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown numericUpDownRed;
+        private System.Windows.Forms.NumericUpDown numericUpDownGreen;
+        private System.Windows.Forms.NumericUpDown numericUpDownBlue;
+    }
+}
+
---
+++ AForgeCameraTracking/AForgeCameraTracking/AForgeCameraTracking/Form1.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AForge;
+using AForge.Video;
+using AForge.Video.DirectShow;
+using AForge.Imaging.Filters;
+using AForge.Imaging;
+using System.Drawing;
+
+namespace AForgeCameraTracking
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private FilterInfoCollection CaptureDevice;
+        private VideoCaptureDevice FinalFrame;
+        private int mode;
+        Bitmap video;
+
+        int red;
+        int green;
+        int blue;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CaptureDevice = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            /**
+             * For Loop getting all the Capture Devices into the drop down Menu
+             * */
+            foreach (FilterInfo Device in CaptureDevice)
+            {
+                deviceBox.Items.Add(Device.Name);
+            }
+            deviceBox.SelectedIndex = 0;
+            FinalFrame = new VideoCaptureDevice();
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            FinalFrame = new VideoCaptureDevice(CaptureDevice[deviceBox.SelectedIndex].MonikerString);
+            FinalFrame.NewFrame += new NewFrameEventHandler(FinalFrame_NewFrame);
+            FinalFrame.Start();
+           
+        }
+
+        /**
+         * Filling out the First PictureBox with the WebCam image
+         **/
+        void FinalFrame_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            video = (Bitmap)eventArgs.Frame.Clone();
+            Bitmap video2 = (Bitmap)eventArgs.Frame.Clone();
+            pictureBox1.Image = video;
+            if(mode == 1)
+            {
+                // Filter Range 
+                ColorFiltering colorFilter = new ColorFiltering();
+                colorFilter.Red = new IntRange(red, 255);
+                colorFilter.Green = new IntRange(green, 75);
+                colorFilter.Blue = new IntRange(blue, 75);
+                colorFilter.ApplyInPlace(video2);
+
+                // Blob counter counts and extracts stand alone objects in images using connected components labelling algortithm.
+                BlobCounter blobCounter = new BlobCounter();
+                blobCounter.MinHeight = 20;
+                blobCounter.MinWidth = 20;
+                blobCounter.ObjectsOrder = ObjectsOrder.Size;
+                blobCounter.ProcessImage(video2);
+
+                Rectangle[] rekt = blobCounter.GetObjectsRectangles();
+                if(rekt.Length > 0)
+                {
+                    Rectangle blobObj = rekt[0];
+                    Graphics graphic = Graphics.FromImage(video2);
+                    using (Pen pen = new Pen(Color.White, 3))
+                    {
+                        graphic.DrawRectangle(pen, blobObj);
+
+                    }
+                    graphic.Dispose();
+                }
+                pictureBox2.Image = video2;
+            }
+            pictureBox1.Image = video;
+
+        }
+
+        /**
+         * Stoping the webcam process
+         * */
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if(FinalFrame.IsRunning == true)
+            {
+                FinalFrame.Stop();
+            }
+        }
+
+        private void buttonCapture_Click(object sender, EventArgs e)
+        {
+            pictureBox2.Image = (Bitmap)pictureBox1.Image.Clone();
+        }
+
+        private void buttonTracking_Click(object sender, EventArgs e)
+        {
+            mode = 1;
+
+        }
+
+        private void trackBarRed_Scroll(object sender, EventArgs e)
+        {
+            red = (int)trackBarRed.Value;
+            numericUpDownRed.Value = red;
+        }
+
+        private void trackBarGreen_Scroll(object sender, EventArgs e)
+        {
+            green = (int)trackBarGreen.Value;
+            numericUpDownGreen.Value = green;
+        }
+
+        private void trackBarBlue_Scroll(object sender, EventArgs e)
+        {
+            blue = (int)trackBarBlue.Value;
+            numericUpDownBlue.Value = blue;
+        }
+    }
+}
